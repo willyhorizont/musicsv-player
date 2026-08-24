917224350f30f1d4af9fd5029ea08ca81af5de28
Willy Horizont <willyhorizont@gmail.com>
2026-08-25 04:01:44 +0700

    [Last updated: 25 Aug 2026 @ 04:01 AM][version: 0.0.4] a fix;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,18 +2,17 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="0.0.3" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="0.0.4" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 
 H="
-[Last updated: $T]
-version $V:
+[Last updated: $T][version: $V]
 "
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
-update readme part 2;
+a fix;
 "
 M=$(sed -e '/./,$!d' <<< "$M")
 M="$H
@@ -24,5 +23,5 @@ git add .
 git commit -m "$M"
 git tag -d "$V" 2>/dev/null
 git tag -a "$V" -m "$M"
-git push origin main -f
-git push origin --tags -f
+git push origin main
+git push origin --tags
